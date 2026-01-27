5449908c87b5609ee0ba82be03345a86365177fd
SJ-Santos <samuel.00000847429@unicap.br>
2026-01-27 11:49:42 -0300

    começo do gerador de questoes

--- core/Question_generator.py
+++ core/Question_generator.py
@@ -1,4 +1,4 @@
 
 def questionario(assunto): # aqui vai ter toda a logica para gerar o questionario em dicionarios e dps vai ser gerado o pdf
-    questoes = [] # lista de questoes em forma de dicionarios"
+    questoes = [] # lista de questoes em forma de dicionarios""
     return questoes
