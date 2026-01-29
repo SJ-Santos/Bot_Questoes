b9868a1f235fe139f4b4382d139c33c1c718d582
SJ-Santos <samuel.00000847429@unicap.br>
2026-01-29 11:37:11 -0300

    começando o uso de IA

--- core/ia_handler.py
+++ core/ia_handler.py
@@ -15,10 +15,26 @@ class IA_Handler:
         )
 
     def gerar_questao(self, assunto: str) -> dict:
-        pass
+        #TODO:colocar os prompts
+        template_gerar_questao = ChatPromptTemplate.from_template([
+            ("system",""),
+            ("human","")
+            ])
+        
+        chain = template_gerar_questao | self.llm.with_structured_output(Questao)
+        return chain.invoke({"assunto": assunto}).dict()
 
-    def corrigir_questao(self, questao: str, resposta_usuario: str) -> str:
-        pass
+    def corrigir_questao(self, questao: dict, resposta_usuario: str) -> str:
+        #TODO:colocar os prompts
+        template_corrigir_questao = ChatPromptTemplate.from_template([
+            ("systen",""),
+            ("human","")
+            ])
+        
+        chain = template_corrigir_questao | self.llm.with_structured_output(CorrecaoQuestao)
+
+        return chain.invoke({"questão":questao,"reposta":resposta_usuario}).dict()
     
-    def ensinar(self, nova_questao: str, resposta_correta: str) -> str:
+    def ensinar(self, assunto) -> str:
+        
         pass
